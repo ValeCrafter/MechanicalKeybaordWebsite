008e59be782422a650b4232807f7926d0a60525a
ValeCrafter <valerio04@gmx.ch>
2023-11-22 16:59:25 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#7D35D4',
+        secondary: '#1D0B32',
+        accent: '#4A1C82',
+        background: '#07030D',
+        text: '#FFFFFF',
+      }
+    },
+  },
+  plugins: [],
+}
