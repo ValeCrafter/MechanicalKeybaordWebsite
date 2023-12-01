b57d05bf1900f34b8cde8b58cf452ba2a0be2830
ValeCrafter <valerio04@gmx.ch>
2023-12-01 11:27:05 +0100

    Revert "formatting and folder structure"
    This reverts commit 248a930d5b88d46c3e10e0ffaf60e5b89ea70fc1.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: "#7D35D4",
-        secondary: "#1D0B32",
-        accent: "#4A1C82",
-        background: "#07030D",
-        text: "#FFFFFF",
+      colors:{
+        primary: '#7D35D4',
+        secondary: '#1D0B32',
+        accent: '#4A1C82',
+        background: '#07030D',
+        text: '#FFFFFF',
       },
     },
   },
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  plugins: [],
+}
